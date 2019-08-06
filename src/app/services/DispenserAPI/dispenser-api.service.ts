9e228f0b21092414530e1282ea9ed6000d043388
Aldy <muhamadaldy17@gmail.com>
2019-08-06 19:26:39 +0800

    dispenser api service ver 2019.08.06

--- src/app/services/DispenserAPI/dispenser-api.service.ts
+++ src/app/services/DispenserAPI/dispenser-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UnitConverter } from 'src/app/classes/UnitConverter/unit-converter';
 
 @Injectable({
   providedIn: 'root'
@@ -348,33 +349,10 @@ export class DispenserAPIService {
     for (let i = 0 ; i < data.length ; i++) {
 
       // if data contains the employee id we need
-      if (data[i]['Employee_ID'] === employee_id) {
+      if (data[i]['Maintainer_ID'] === employee_id) {
 
         // if data contains status above-equal to 5 and has repair time done
         if (data[i]['Status'] >= 5 && data[i]['RepairDoneTime'] !== "") {
-          
-          returnArray.push(data[i]);
-        }  
-      }
-    }
-
-    return returnArray;
-  }
-
-  async getAssignmentNext (device_id: string, employee_id: string) {
-    
-    // get data from RepairCondition
-    let data = await this.getDispenserRepairCondition(device_id);
-    let returnArray = [];
-
-    // for every data will be filtered to get what have been done
-    for (let i = 0 ; i < data.length ; i++) {
-
-      // if data contains the employee id we need
-      if (data[i]['Employee_ID'] === employee_id) {
-
-        // if data contains status = 4 but has repair time done
-        if (data[i]['Status'] === 4 && data[i]['RepairDoneTime'] !== "") {
           
           returnArray.push(data[i]);
         }  
@@ -394,13 +372,13 @@ export class DispenserAPIService {
     for (let i = 0 ; i < data.length ; i++) {
 
       // if data contains the employee id we need
-      if (data[i]['Employee_ID'] === employee_id) {
+      if (data[i]['Maintainer_ID'] === employee_id) {
 
-        // if data contains status = 5 and has repair time done
-        if (data[i]['Status'] === 4 && data[i]['RepairDoneTime'] !== "") {
+        // if data contains status = 4 and repair call time
+        if (data[i]['Status'] === 4 && data[i]['RepairCallTime'] !== "") {
 
-          let missionTime = this.convertApiTimeToJson(data[i]['RepairDoneTime']);
-          let currentTime = this.convertApiTimeToJson(nowTime);
+          let missionTime = UnitConverter.convertApiTimeToJson(data[i]['RepairCallTime']);
+          let currentTime = UnitConverter.convertApiTimeToJson(nowTime);
           
           // if data has same day as mission deadline
           if (
@@ -408,15 +386,47 @@ export class DispenserAPIService {
             missionTime['Month'] === currentTime['Month'] &&
             missionTime['DateOfMonth'] === currentTime['DateOfMonth']
           ) {
+
+            let dateCurrentTime = UnitConverter.convertApiTimeToDate(nowTime).getTime();
+            let dateMissionTime = UnitConverter.convertApiTimeToDate(data[i]['RepairCallTime']).getTime();
             
             // if data is under the deadline time
-            if (missionTime['Hour'] <= currentTime['Hour'] &&
-                missionTime['Minute'] <= currentTime['Minute'] &&
-                missionTime['Second'] <= currentTime['Second']
-            ) {
-              
+            if (dateCurrentTime <= dateMissionTime) {
+
               // put into data will be returned
               returnArray.push(data[i]);
+            }
+          }
+        }  
+      }
+    }
+
+    return returnArray;
+  }
+
+  async getAssignmentNext (device_id: string, employee_id: string, nowTime: string) {
+    
+    // get data from RepairCondition
+    let data = await this.getDispenserRepairCondition(device_id);
+    let returnArray = [];
+
+    // for every data will be filtered to get what have been done
+    for (let i = 0 ; i < data.length ; i++) {
+
+      // if data contains the employee id we need
+      if (data[i]['Maintainer_ID'] === employee_id) {
+
+        // if data contains status = 4 and repair call time
+        if (data[i]['Status'] === 4 && data[i]['RepairCallTime'] !== "") {
+          
+          let missionTime = UnitConverter.convertApiTimeToJson(data[i]['RepairCallTime']);
+          let currentTime = UnitConverter.convertApiTimeToJson(nowTime);
+
+          if (missionTime['Year'] >= currentTime['Year']) {
+            if (missionTime['Month'] >= currentTime['Month']) {
+              if (missionTime['DateOfMonth'] > currentTime['DateOfMonth']) {
+                returnArray.push(data[i]);
+              }
             }
           }
         }  
@@ -434,77 +444,5 @@ export class DispenserAPIService {
 
   }
 
-  /**
-   * This function is to convert the time from the API into Date
-   * object in typescript library. The purpose is to create a new
-   * Date object with data from the API. This function will return
-   * Date object
-   * 
-   * @param     time    Value of time from API in "2019-03-08 16:32:00" format
-   * 
-   * @returns   Date    Date object converted result from time
-   * 
-   * @example
-   * 
-   * {
-   *    "Year": 2019,
-   *    "Month": 2,
-   *    "DateOfMonth": 14,
-   *    "Hour": 19,
-   *    "Minute": 44,
-   *    "Second": 11,
-   * }
-   */
-  convertApiTimeToJson (time: string) {
-    // time passed is String, construct into Date format
-    // time example from json: "2019-03-08 16:32:00"
-    // format: YEAR-MONTH-DATEOFMONTH HOUR:MINUTE:SECOND
-    
-    // split into DATE form and HOUR form
-    let splitTime = time.split(" ");
-
-      ////////////////////////////////////////////
-     //  DATE PART                             //
-    ////////////////////////////////////////////
-
-    // resultDate = YEAR-MONTH-DATEOFMONTH
-    let resultDate = splitTime[0];
-
-    // split DATE into YEAR, MONTH, and DATEOFMONTH
-    let splitDate = resultDate.split("-");
-
-    let resultYear = splitDate[0];
-    let resultMonth = splitDate[1];
-    let resultDateOfMonth = splitDate[2];
-
-      ////////////////////////////////////////////
-     //  HOUR PART                             //
-    ////////////////////////////////////////////
-
-    // resultHour = HOUR:MINUTE:SECOND
-    let resultHour = splitTime[1];
-
-    // split HOUR into HOUR, MINUTE, and SECOND
-    let splitHour = resultHour.split(":");
-
-    let resultHourC = splitHour[0];
-    let resultMinute = splitHour[1];
-    let resultSecond = splitHour[2];
-
-      ////////////////////////////////////////////
-     //  CONSTRUCT DATE PART                   //
-    ////////////////////////////////////////////
-
-    // now we get every component to construct date into JSON format
-    let newDate = {
-      'Year': resultYear,
-      'Month': resultMonth,
-      'DateOfMonth': resultDateOfMonth,
-      'Hour': resultHourC,
-      'Minute': resultMinute,
-      'Second': resultSecond
-    };
-
-    return newDate;
-  }
+  
 }
