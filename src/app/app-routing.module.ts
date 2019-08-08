2d1c55a4a2dbaa33cc879f72ed22abccc33dcdcc
Aldy <muhamadaldy17@gmail.com>
2019-08-08 15:08:59 +0800

    remove ver 2019.08.08
    - remove modal-page.page
    - remove test.page
    - update app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'test', loadChildren: './pages/test/test.module#TestPageModule' },
-  { path: 'modal-page', loadChildren: './pages/modal-page/modal-page.module#ModalPagePageModule' },
   { path: 'detail', loadChildren: './pages/detail/detail.module#DetailPageModule' },
   { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
-
+  { path: 'forgot-password', loadChildren: './pages/auth/forgot-password/forgot-password.module#ForgotPasswordPageModule' },
+  { path: 'reset-password', loadChildren: './pages/auth/reset-password/reset-password.module#ResetPasswordPageModule' },
 ];
 
 @NgModule({
