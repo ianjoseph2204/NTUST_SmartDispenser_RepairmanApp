b47d196b105db7ba2f0834e61b9cffeae840b89b
Aldy <muhamadaldy17@gmail.com>
2019-08-08 17:39:10 +0800

    reset-password ver 2019.08.08
    - add page
    - add back end with api
    - test success

--- src/app/pages/auth/reset-password/reset-password.page.ts
+++ src/app/pages/auth/reset-password/reset-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DispenserAPIService } from 'src/app/services/DispenserAPI/dispenser-api.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  // form attributes
+  credential = "";
+  new_password = "";
+  re_new_password = "";
+  verif_code = "";
+
+  constructor(
+    private api: DispenserAPIService
+  ) { }
 
   ngOnInit() {
   }
 
+  async reset () {
+
+    const { credential, new_password, re_new_password, verif_code } = this;
+
+    if (
+      credential === "" ||
+      new_password === "" ||
+      re_new_password === "" ||
+      verif_code === ""
+    ) {
+
+    } else {
+      let resultData: any;
+      resultData = await this.api.resetPasswordUsingEmail(credential, new_password, re_new_password, verif_code);
+
+      if (resultData === 0 || resultData === -1)
+        resultData = await this.api.resetPasswordUsingEmployeeId(credential, new_password, re_new_password, verif_code);
+
+      console.log(resultData);
+    }
+  }
+
+  backFunc () {
+    
+  }
 }
