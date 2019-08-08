4bd2e246f1f67fab1c717b2b05124ce09c727fad
Aldy <muhamadaldy17@gmail.com>
2019-08-08 17:38:05 +0800

    forgot-password ver 2019.08.08
    - add page
    - add back end with api
    - test success

--- src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DispenserAPIService } from 'src/app/services/DispenserAPI/dispenser-api.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPage implements OnInit {
 
-  constructor() { }
+  credential = "";
+
+  constructor(
+    private api: DispenserAPIService
+  ) { }
 
   ngOnInit() {
   }
 
+  async continue () {
+
+    const {credential} = this;
+
+    if (credential === "") {
+
+    } else {
+      let resultData: any;
+      resultData = await this.api.forgotPasswordUsingEmail(credential);
+
+      if (resultData === 0 || resultData === -1)
+        resultData = await this.api.forgotPasswordUsingEmployeeId(credential);
+
+      console.log(resultData);
+    }
+  }
+
+  backFunc () {
+    
+  }
 }
