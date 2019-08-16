f11c4329bf2f67dc09947ef57a61d9f22769e45c
Aldy <muhamadaldy17@gmail.com>
2019-08-16 15:31:17 +0800

    forgot-password.page ver 2019.08.16
    - add NavCtrl, ToastCtrl, LoadCtrl
    - add loading before and after process
    - update UI so more dynamic

--- src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DispenserAPIService } from 'src/app/services/DispenserAPI/dispenser-api.service';
+import { NavController, ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,33 +9,99 @@ import { DispenserAPIService } from 'src/app/services/DispenserAPI/dispenser-api
 })
 export class ForgotPasswordPage implements OnInit {
 
+  // field variable to store input
   credential = "";
 
+  // loadCtrl var
+  makeLoading: any;
+
   constructor(
-    private api: DispenserAPIService
+    private navCtrl: NavController,
+    private api: DispenserAPIService,
+    private toastCtrl: ToastController,
+    private loadCtrl: LoadingController
   ) { }
+
+  /**
+   * To going back, or route back, to the previous
+   * opened page.
+   */
+  backFunc() {
+    this.navCtrl.back();
+  }
+
+  /**
+   * Create the loading controller
+   */
+  async createLoadCtrl () {
+
+    // insert component of loading controller
+    this.makeLoading = await this.loadCtrl.create({
+      message: 'Loading data ...',
+      spinner: 'crescent',
+      duration: 10000
+    });
+
+    // display the loading controller
+    await this.makeLoading.present();
+  }
+
+  /**
+   * Dismiss the loading controller
+   */
+  async dismissLoadCtrl () {
+    this.makeLoading.dismiss();
+  }
 
   ngOnInit() {
   }
 
-  async continue () {
+  async reset (hasCredential: boolean) {
 
-    const {credential} = this;
+    if (!hasCredential){
 
-    if (credential === "") {
+      // if user click RESET button
+      this.navCtrl.navigateForward(['reset-password']);
 
     } else {
-      let resultData: any;
-      resultData = await this.api.forgotPasswordUsingEmail(credential);
 
-      if (resultData === 0 || resultData === -1)
-        resultData = await this.api.forgotPasswordUsingEmployeeId(credential);
+      // initial local variables
+      let myToast: any;
+      let myToastMessage: string = "";
+      const {credential} = this;
 
-      console.log(resultData);
+      if (credential === "") {
+        myToastMessage = "Please fill in all the required form!"
+      } else {
+        
+        // create loading screen
+        await this.createLoadCtrl();
+
+        let resultData = await this.api.forgotPassword(credential);
+        if (resultData === 1) {
+          myToastMessage = "Verification code has been sent to your email address!";
+          this.navCtrl.navigateForward(['reset-password']);
+        } else if (resultData === 0) {
+          myToastMessage = "Email address or employee ID is not found, please try again!";
+        } else {
+          myToastMessage = "There is an unexpected error, please try again later!";
+        }
+      }
+
+      // create Toast with myToastMessage as message display
+      myToast = await this.toastCtrl.create({
+        message: myToastMessage,
+        duration: 2000,
+        position: 'top',
+        showCloseButton: true,
+        closeButtonText: 'Close'
+      });
+
+      // display the Toast
+      await myToast.present();
+
+      // dismiss the loading screen
+      this.dismissLoadCtrl();
     }
-  }
-
-  backFunc () {
-    
   }
 }
